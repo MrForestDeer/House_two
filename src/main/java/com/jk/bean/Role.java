ed7443e9e35d313120e1e717c4f1557c60e89db8
13828839917 <1056948767@qq.com>
2019-05-07 21:38:43 +0800

    第一次人员管理

--- src/main/java/com/jk/bean/Role.java
+++ src/main/java/com/jk/bean/Role.java
@@ -2,9 +2,11 @@ package com.jk.bean;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class Role {
     private Integer rid; //主键
     private String rolename;//角色名称
-
+    private Date  createDate;//创建时间****************
 }
