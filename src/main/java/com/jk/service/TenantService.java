9dad32b45baf4b3554270763fa5cbf20d1e28dde
9790926@qq.com <97909256@qq.com>
2019-05-07 19:33:26 +0800

    第一次提交租房合同

---
+++ src/main/java/com/jk/service/TenantService.java
@@ -0,0 +1,23 @@
+package com.jk.service;
+
+import com.jk.bean.Room;
+import com.jk.bean.Tenant;
+
+import java.util.HashMap;
+
+public interface TenantService {
+    HashMap<String, Object> queryTenant(Tenant tenant,Integer page, Integer rows);
+
+    HashMap<String, Object> queryRoom(Room room,Integer page, Integer rows);
+
+
+    String queryTenantById(Tenant tenant, Integer id);
+
+    String queryRoomById(Room room, Integer id);
+
+    void updateRoom(Room room);
+
+    Room findroombyid(Integer id);
+
+    void addRoom(Room room);
+}
